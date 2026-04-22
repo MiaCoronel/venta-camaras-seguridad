bbb7ed7060b478c7c2b1a9f1d2388a84022609d0
MiaCoronel <coroneluchuypomamianicoll@gmail.com>
2026-04-22 02:15:14 -0500

    chore: inicializar proyecto Spring Boot con estructura base

---
+++ src/main/java/com/ventacamaras/camaras/VentaCamarasApplication.java
@@ -0,0 +1,14 @@
+package com.ventacamaras.camaras;
+
+import java.util.Collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentaCamarasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentaCamarasApplication.class, args);	
+	}
+}
