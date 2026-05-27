bd9c5d5290fd187753e7f795bef5edb9cfb49c22
Mia#c <coroneluchuypomamianicoll@gmail.com>
2026-05-27 00:13:31 -0500

    Create CamaraImagen.java

---
+++ src/main/java/com/ventacamaras/camaras/model/CamaraImagen.java
@@ -0,0 +1,29 @@
+package com.ventacamaras.camaras.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "camara_imagenes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CamaraImagen {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "camara_id", nullable = false)
+    private Camara camara;
+
+    @Column(nullable = false, length = 500)
+    private String url;
+
+    @Column(name = "num_imagen", nullable = false)
+    private Integer numImagen;
+
+    @Column(name = "es_principal", nullable = false)
+    private Boolean esPrincipal;
+}
